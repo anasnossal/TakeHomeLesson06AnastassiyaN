1d1a49506bdb5995aa060c7cfa5fcb5eac35a23c
anastasianossal <an2832@tc.columbia.edu>
2018-03-04 23:03:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/takehomelesson06_anastassiyan/Keys.java
@@ -0,0 +1,9 @@
+package com.example.android.takehomelesson06_anastassiyan;
+
+/**
+ * Created by anastasianossal on 3/3/18.
+ */
+
+public interface Keys { String TIP_KEY = "lalala";
+
+}
---
+++ app/src/main/java/com/example/android/takehomelesson06_anastassiyan/ReceiptActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.takehomelesson06_anastassiyan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceiptActivity extends AppCompatActivity {
+    TextView one;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt);
+
+
+    one = (TextView) findViewById(R.id.receipt_summary);
+
+    Intent intent = getIntent();
+    TipCalculator myCalc = (TipCalculator) intent.getSerializableExtra(Keys.TIP_KEY);
+       one.setText(myCalc.toString());
+}
+}
+
